6fc07015dd3b089905fcf76694529bf53d8f0ba2
mertunveren4545-collab <mertunveren4545@gmail.com>
2026-08-04 00:19:46 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const SURUM = 'opmuk-s10';
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.matchAll({type:'window', includeUncontrolled:true}).then(list => {
+    for (const c of list) { if ('focus' in c) return c.focus(); }
+    if (clients.openWindow) return clients.openWindow('./');
+  }));
+});
